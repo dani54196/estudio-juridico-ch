404a52cac0501fab0bd186511d0d2663426099a7
dani54196 <dani54196@gmail.com>
2024-03-27 10:02:09 -0300

    wp bottom

--- src/components/ContentBlock/RightContentBlock/index.tsx
+++ src/components/ContentBlock/RightContentBlock/index.tsx
@@ -4,6 +4,7 @@ import { SvgIcon } from "../../../common/SvgIcon";
 import { Button } from "../../../common/Button";
 import { ContentBlockProps } from "../types";
 import { Fade } from "react-awesome-reveal";
+
 import {
   RightBlockContainer,
   Content,
@@ -31,26 +32,20 @@ const RightBlock = ({
         <Row justify="space-between" align="middle" id={id}>
           <Col lg={11} md={11} sm={11} xs={24}>
             <ContentWrapper>
-              <h1>Estudio Juridico</h1>
-              <Content>
-                ¿ESTÁS EN BUSCA DE <b>ABOGADOS</b> CONFIABLES Y COMPROMETIDOS?
-              </Content>
-              <Content>Mas de 5 años de experiencia. </Content>
-              <Content>Mas de 50 clientes satisfechos.</Content>
+              <h1>{t(title)}</h1>
+              <Content>{t(content)}</Content>
               <ButtonWrapper>
-                {typeof button === "object" &&
-                  button.map((item: any, id: number) => {
-                    return (
-                      <Button
-                        key={id}
-                        color={item.color}
-                        fixedWidth={true}
-                        onClick={() => scrollTo("about")}
-                      >
-                        {t(item.title)}
-                      </Button>
-                    );
-                  })}
+                <a
+                  href="https://api.whatsapp.com/message/INDLYWFEXOD3B1?autoload=1&app_absent=0"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="https://canacintra-leon.org.mx/wp-content/uploads/2022/06/boton-whatsapp.png"
+                    alt="whatsapp contact"
+                    width={250}
+                  />
+                </a>
               </ButtonWrapper>
             </ContentWrapper>
           </Col>
