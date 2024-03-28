7fb00aec46b2f1f4c96e1c5978eb36d4662a40f3
dani54196 <dani54196@gmail.com>
2024-03-28 10:12:49 -0300

    fix error from fetch api, reorden structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 
-import "./Utility/Review"
-import "./Utility/Instagram"
-
 import Router from "./router";
 
 const App = () => (
